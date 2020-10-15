995bd67421f4c7b6ba2c569620172ee2cc92a327
wlzhaq <631572878@qq.com>
2020-10-15 16:52:29 +0800

    Add files via upload

---
+++ btc_close_2017.py
@@ -0,0 +1,41 @@
+import json
+import pygal
+import math
+
+
+filename = 'btc_close_2017_urllib.json'
+with open(filename) as f:
+    btc_data = json.load(f)
+
+for btc_dict in btc_data:
+    data = btc_dict['date']
+    month = int(btc_dict['month'])
+    week = int(btc_dict['week'])
+    weekday = btc_dict['weekday']
+    close = int(float(btc_dict['close']))
+    print("{} is month {}week {},{},the close price is {} RMB".format(data, month, week, weekday, close))
+
+dates = []
+months = []
+weeks = []
+weekdays = []
+close = []
+for btc_dict in btc_data:
+    dates.append(btc_dict['date'])
+    months.append(int(btc_dict['month']))
+    weeks.append(int(btc_dict['week']))
+    weekdays.append(btc_dict['weekday'])
+    close.append(int(float(btc_dict['close'])))
+
+
+line_chart = pygal.Line(x_label_rotation=20, show_minor_x_labels=False)
+line_chart.title = '收盘价对数变换(￥)'
+line_chart.x_labels = dates
+N = 20
+line_chart._x_labels_major = dates[::N]
+close_log =[ math.log10(_) for _ in close]
+line_chart.add('log收盘价', close_log)
+line_chart.render_to_file('收盘价对数变换折线图(￥).svg')
+
+
+
